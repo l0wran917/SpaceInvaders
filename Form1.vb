d2482454fa9ef153b2790341f3ba00a4a8d71ff4
Laurent <laurent@bassin.info>
2015-06-04 07:42:31 +0200

    Init commit

---
+++ Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+
+#Region "Classes"
+
+    ''' <summary>
+    ''' Classe representant le vaisseau du joueur
+    ''' </summary>
+    ''' <remarks></remarks>
+    Class joueur
+        Private position As New Point()
+        Private taille As New Size()
+
+    End Class
+
+#End Region
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
