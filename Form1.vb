65f52ef32d5fc7969937bb978d611fe19d2fbc6e
Laurent <laurent@bassin.info>
2015-06-04 08:00:21 +0200

    Ajout classe joueur + affichage img

--- Form1.vb
+++ Form1.vb
@@ -6,15 +6,35 @@
     ''' Classe representant le vaisseau du joueur
     ''' </summary>
     ''' <remarks></remarks>
-    Class joueur
-        Private position As New Point()
-        Private taille As New Size()
+    Class Joueur
+        Private position As New Point() ' Position du joueur
+        Private taille As New Size()    ' Taille du joueur
+
+        Private img As New PictureBox() ' Img du joueur
+
+        Private scrore As Integer       ' Score du joueur
+
+        Sub New(ByVal pos As Point, ByVal tailleJoueur As Size) ' Constructeur
+            position = pos
+            taille = tailleJoueur
+
+            img.Image = Image.FromFile("../../img/vaisseau.png")
+        End Sub
+
+        Function getPictureBox()        ' Retourne pictureBox
+            img.Location = position
+            Return img
+        End Function
 
     End Class
 
 #End Region
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim joueur As New Joueur(New Point(50, 50), New Size(68, 49))
 
+        Me.Controls.Add(joueur.getPictureBox())
     End Sub
+
+
 End Class
