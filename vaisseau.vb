e2f9609f053b40704c92e8e0b5eb0c7eb5f8fe5a
Laurent <laurent@bassin.info>
2015-06-10 10:48:16 +0200

    Commentaire Vaisseau

--- vaisseau.vb
+++ vaisseau.vb
@@ -1,11 +1,15 @@
 ﻿Public Class Vaisseau
     Inherits PictureBox
 
-    Dim vitesse As Integer = 20
-    Public missile As Missile
+    Dim vitesse As Integer = 20 ' Vitesse de deplacement
+    Public missile As Missile ' Declare une reference vers 1 missile
 
-    Dim score As Integer = 0
+    Dim score As Integer = 0 ' Score actuel
 
+    ''' <summary>
+    ''' Constructeur (Charge img + position)
+    ''' </summary>
+    ''' <remarks></remarks>
     Sub New()
         Image = Image.FromFile("../../img/vaisseau.png")
         Size = Image.Size
@@ -13,6 +17,11 @@
         Location = New Point(10, Form1.pnlJeu.Height - Image.Height - 55) ' 15 px du bord inferieur de la fenetre
     End Sub
 
+    ''' <summary>
+    ''' Deplace le vaisseau sur l'axe X en fonction de la direction (en fonction de la touche pressée)
+    ''' </summary>
+    ''' <param name="direction">Direction deplacement (Droite : 1 / Gauche : -1)</param>
+    ''' <remarks></remarks>
     Sub deplacer(ByVal direction As Integer)
         Dim pos = Location
         Dim deplacement As Integer = vitesse * direction
@@ -24,6 +33,10 @@
         Location = New Point(Location.X + deplacement, Location.Y)
     End Sub
 
+    ''' <summary>
+    ''' Genere un missile (Après appui sur touche)
+    ''' </summary>
+    ''' <remarks></remarks>
     Sub tirer()
         If (missile Is Nothing) Then
             Dim missile As New Missile(Me)
@@ -33,12 +46,21 @@
         End If
     End Sub
 
+    ''' <summary>
+    ''' Fait avancer le missile s'il existe
+    ''' </summary>
+    ''' <remarks></remarks>
     Sub deplacerMissile()
         If (Not missile Is Nothing) Then
             missile.deplacer()
         End If
     End Sub
 
+    ''' <summary>
+    ''' Supprime le missile du panel de jeu s'il existe
+    ''' </summary>
+    ''' <param name="ennemiTue">Si le missile a été un ennemi ou non (augmentation du score)</param>
+    ''' <remarks></remarks>
     Sub supprimerMissile(ByVal ennemiTue As Boolean)
         If (ennemiTue) Then
             score += 10
@@ -48,10 +70,21 @@
         missile = Nothing
     End Sub
 
+    ''' <summary>
+    ''' Retourne sour forme de string le score à afficher
+    ''' </summary>
+    ''' <returns>Chaine de caracter de la forme => Score : x</returns>
+    ''' <remarks></remarks>
     Function getScore()
         Return "Score : " + score.ToString()
     End Function
 
+
+    ''' <summary>
+    ''' Retourne la zone en bas de l'ecran où le joueur se deplace
+    ''' </summary>
+    ''' <returns>Rectangle de la zone de deplacement du joueur</returns>
+    ''' <remarks></remarks>
     Function getZoneJeu()
         Dim zoneJoueur As New Rectangle()
         zoneJoueur.X = 0
@@ -60,7 +93,6 @@
         zoneJoueur.Height = Form1.pnlJeu.Width - (Form1.pnlJeu.Height - Me.Location.Y)
 
         Return zoneJoueur
-
     End Function
 
 End Class
