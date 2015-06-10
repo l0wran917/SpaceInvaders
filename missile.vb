92b3a3e9341aca929a29e7208b2f0b5d2071dce5
Laurent <laurent@bassin.info>
2015-06-10 10:44:20 +0200

    Commentaire Missile

--- missile.vb
+++ missile.vb
@@ -1,9 +1,14 @@
 ﻿Public Class Missile
     Inherits PictureBox
 
-    Dim vitesse As Integer = 15
-    Dim tireur As Vaisseau
+    Dim vitesse As Integer = 15 ' Vitesse de deplacement
+    Dim tireur As Vaisseau ' Reference vers le vaisseau qui lance le missile
 
+    ''' <summary>
+    ''' Constructeur (charge img + position)
+    ''' </summary>
+    ''' <param name="tireur"></param>
+    ''' <remarks></remarks>
     Sub New(ByVal tireur As Vaisseau)
         Image = Image.FromFile("../../img/missile.png")
         Size = Image.Size
@@ -13,6 +18,10 @@
         Me.tireur = tireur
     End Sub
 
+    ''' <summary>
+    ''' Deplace le missile en Y en fonction de sa vitesse
+    ''' </summary>
+    ''' <remarks></remarks>
     Sub deplacer()
         If (Location.Y > 0) Then
             Location = New Point(Location.X, Location.Y - vitesse)
